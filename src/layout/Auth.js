eec906fb868110ffe4e9c99f78f7d8f2f5f6b7c9
Rudolf Benadie <rudolf.benadie@hotmail.com>
2020-11-05 19:15:20 +0000

    Add side bar component

--- src/layout/Auth.js
+++ src/layout/Auth.js
@@ -2,15 +2,12 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 
 const AuthLayout = (props) => {
-  if (!props.currentUser) {
-    return (
-      <div>
-        <h1>Authorisation layout</h1>
-      </div>
-    )
-  } else {
-    return <Redirect to="/" />
-  }
+  if (props.currentUser) return <Redirect to="/" />
+  return (
+    <div>
+      <h1>Authorisation layout</h1>
+    </div>
+  )
 }
 
 export default AuthLayout;
