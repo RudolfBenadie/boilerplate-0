61101fb3ad51280e7f24b8ae637542136515e05f
Rudolf Benadie <rudolf.benadie@hotmail.com>
2020-11-06 20:55:40 +0000

    Start routes

--- src/layout/SideBar.js
+++ src/layout/SideBar.js
@@ -1,14 +1,46 @@
 import React from "react";
+import {
+  Nav,
+  NavLink
+} from "reactstrap";
 
 const SideBar = (props) => {
+  function activeRoute(routeName) {
+    return props.location && props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  }
+  var sidebar = React.createRef();
   return (
     <div id="sidebar">
       <div className="header">
         <p>Choose an option:</p>
       </div>
-      <div id="nav">
-        <h3>Dashboard</h3>
+
+      <div className="sidebar-wrapper" ref={sidebar}>
+        <Nav>
+          {props.routes.map((prop, key) => {
+            return (
+              <li
+                className={
+                  activeRoute(prop.path) +
+                  (prop.pro ? " active-pro" : "")
+                }
+                key={key}
+              >
+                <NavLink
+                  to={prop.layout + prop.path}
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  {/* <i className={prop.icon} /> */}
+                  {/* <FontAwesomeIcon icon={prop.icon} /> */}
+                  <p>{prop.name}</p>
+                </NavLink>
+              </li>
+            );
+          })}
+        </Nav>
       </div>
+
     </div>
   )
 }
