68b46267db7fd4000de420463e8675ccd35a05eb
Rudolf Benadie <rudolf.benadie@hotmail.com>
2020-11-07 11:39:17 +0000

    Fix routing, styling

---
+++ src/views/dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = (props) => {
+  return (
+    <div id="dashboard" className="content" >
+      <h1>Dashboard layout</h1>
+    </div>
+  )
+}
+
+export default Dashboard;
+
